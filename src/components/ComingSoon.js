70e53a3b13f75dd5f8b04f8e91a504bc25ae92d0
Dembe Kwinda <deekwinda@icloud.com>
2025-07-23 22:31:24 +0200

    Initial commit - setup mr-stepup sneaker store project

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -4,8 +4,8 @@ import "./ComingSoon.css";
 export default function ComingSoon() {
   return (
     <section className="coming-soon-section">
-      <h1>🚧 Coming Soon</h1>
-      <p>We're working on something exciting! Stay tuned for updates.</p>
+      <h1 className="glow-text">Coming Soon</h1>
+      <p>We're working hard to bring you something awesome. Stay tuned!</p>
     </section>
   );
 }
